2ad87f528516681377817ea1325fa82b3192edb2
Your Name <you@example.com>
2018-09-15 16:38:05 +0300

    add login module with login page component

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
