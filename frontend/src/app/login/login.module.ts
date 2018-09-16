c348b61a22809a2bd1fc24d6d45604b08e5d3835
Your Name <you@example.com>
2018-09-16 14:31:28 +0300

    add register-page

--- frontend/src/app/login/login.module.ts
+++ frontend/src/app/login/login.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
-  declarations: [LoginPageComponent],
+  declarations: [LoginPageComponent, RegisterPageComponent],
   exports: [LoginPageComponent]
 })
 export class LoginModule { }
